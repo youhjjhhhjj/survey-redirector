3530d94bb67c246237d123164dd6d3017b418e5a
youhjjhhhjj <allenli.comix@gmail.com>
2023-11-18 16:12:32 -0500

    restructure index.js

--- index.js
+++ index.js
@@ -3,57 +3,60 @@ const fs = require('fs');
 const path = require('path');
 const port = process.env.PORT || 3000;
 
-http.createServer(function (request, response) {
+const mimeTypes = {
+    '.html': 'text/html',
+    '.js': 'text/javascript',
+    '.css': 'text/css',
+    '.json': 'application/json',
+    '.png': 'image/png',
+    '.jpg': 'image/jpg',
+    '.gif': 'image/gif',
+    '.svg': 'image/svg+xml',
+    '.wav': 'audio/wav',
+    '.mp4': 'video/mp4',
+    '.woff': 'application/font-woff',
+    '.ttf': 'application/font-ttf',
+    '.eot': 'application/vnd.ms-fontobject',
+    '.otf': 'application/font-otf',
+    '.wasm': 'application/wasm',
+    '.ico' : 'image/x-icon'
+};
+const staticPaths = new Set(['/', '/style.css', '/script.js']);
+
+
+function loadFile(filePath) {
+    try {
+        let content = fs.promises.readFile(filePath, 'utf8');
+        return content;
+    }
+    catch(err) {
+        console.error("Error on serving " + filePath);
+        return null;
+    }
+}
+
+http.createServer(async function (request, response) {
     let url = new URL("http://" + request.headers.host + request.url);
-    let filePath = url.pathname;
-    console.log('request: ' + filePath);
+    let subPath = url.pathname;
+    console.log('request: ' + subPath);
 
-    if (filePath == '/') {
-        filePath = 'public/index.html';
-    }
-    else {
-        filePath = 'public' + request.url;
-    }
-
-    let extname = String(path.extname(filePath)).toLowerCase();
-    let mimeTypes = {
-        '.html': 'text/html',
-        '.js': 'text/javascript',
-        '.css': 'text/css',
-        '.json': 'application/json',
-        '.png': 'image/png',
-        '.jpg': 'image/jpg',
-        '.gif': 'image/gif',
-        '.svg': 'image/svg+xml',
-        '.wav': 'audio/wav',
-        '.mp4': 'video/mp4',
-        '.woff': 'application/font-woff',
-        '.ttf': 'application/font-ttf',
-        '.eot': 'application/vnd.ms-fontobject',
-        '.otf': 'application/font-otf',
-        '.wasm': 'application/wasm',
-        '.ico' : 'image/x-icon'
-    };
-
-    let contentType = mimeTypes[extname] || 'application/octet-stream';
-
-    fs.readFile(filePath, function(error, content) {
-        if (error) {
-            if(error.code == 'ENOENT') {
-                response.writeHead(404);
-                response.end('Resource not found.\n');
-
-            }
-            else {
-                response.writeHead(500);
-                response.end('An unexpected error was encountered.\n');
-            }
+    if (staticPaths.has(subPath)) {
+        if (subPath == '/') subPath = '/index.html';
+        let content = await loadFile("public" + subPath);
+        if (content === null) {
+            response.writeHead(500);
+            response.end('An unexpected error was encountered.\n');
         }
         else {
+            let extname = String(path.extname(subPath)).toLowerCase();
+            let contentType = mimeTypes[extname] || 'application/octet-stream';
             response.writeHead(200, { 'Content-Type': contentType });
-            response.end(content, 'utf-8');
+            response.end(content, 'utf-8');            
         }
-    });
-
+    }
+    else {
+        response.writeHead(404);
+        response.end('Resource not found.\n');
+    }
 }).listen(port);
 console.log(`Server running on ${port}`);
